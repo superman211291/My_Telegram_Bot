d5aaa07aea273caaaa63fbe185f547cb81021d77
Ilya Stroev <ilya_stroev@mail.ru>
2025-02-19 21:33:29 +0300

    add code bot 2

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import telebot
-
 # токен бота
-bot = telebot.TeleBot("7947969538:AAGeRi_1b7E-AbidZxvhfjsJ06Mn9UHDu30")
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я не знаю такой команды. Напиши /help.")
-        bot.polling(none_stop=True, interval=0)
+botTimeWeb  = telebot.TeleBot("7947969538:AAGeRi_1b7E-AbidZxvhfjsJ06Mn9UHDu30")
+from telebot import types
+
+@botTimeWeb.message_handler(commands=['start'])
+def startBot(message):
+  first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь расскажу немного о нашей компании?"
+  markup = types.InlineKeyboardMarkup()
+  button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
+  markup.add(button_yes)
+  botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+
+@botTimeWeb.callback_query_handler(func=lambda call: True)
+def response(function_call):
+    if function_call.message:
+        if function_call.data == "yes":
+            second_mess = "Мы облачная платформа для разработчиков и бизнеса. Более детально можешь ознакомиться с нами на нашем сайте!"
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton("Перейти на сайт", url="https://timeweb.cloud/"))
+            botTimeWeb.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
+            botTimeWeb.answer_callback_query(function_call.id)
+botTimeWeb.infinity_polling()
